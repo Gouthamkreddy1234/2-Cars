4486a9b4b8aeeba3974856344951a600958d89ec
Harish Kandala <kandalaharish95@gmail.com>
2016-03-09 02:39:31 +0530

    cars added

--- js/app.js
+++ js/app.js
@@ -7,6 +7,10 @@ var WINDOW_WIDTH = window.innerWidth;
 var WINDOW_HEIGHT = window.innerHeight;
 var BACKGROUND_COLOR = "#25337a";
 var LINE_COLOR = "#6572a7";
+var RED_CAR = new Image();
+RED_CAR.src = "../img/redcar.png";
+var BLUE_CAR = new Image();
+BLUE_CAR.src = "../img/bluecar.png";
 
 window.onload = function () {
     var canvas = document.getElementById("myCanvas");
@@ -21,4 +25,10 @@ window.onload = function () {
     ctx.fillRect(Math.floor(WINDOW_WIDTH/2) + 100, 0, 2, WINDOW_HEIGHT);
     ctx.fillRect(Math.floor(WINDOW_WIDTH/2) - 200, 0, 2, WINDOW_HEIGHT);
     ctx.fillRect(Math.floor(WINDOW_WIDTH/2) + 200, 0, 2, WINDOW_HEIGHT);
+
+
+    /*var redcar = document.getElementById("red-car");
+    ctx.drawImage(redcar, Math.floor(WINDOW_WIDTH/2) - 180 , Math.floor(WINDOW_HEIGHT) - 180);
+    var bluecar = document.getElementById("blue-car");
+    ctx.drawImage(bluecar, Math.floor(WINDOW_WIDTH/2) + 120 , Math.floor(WINDOW_HEIGHT) - 180);*/
 };
