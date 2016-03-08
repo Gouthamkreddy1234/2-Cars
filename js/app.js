8f945d8d8b57ef929fb18c1a65b150bec0031b58
Harish Kandala <kandalaharish95@gmail.com>
2016-03-09 02:04:29 +0530

    basic layout

--- js/app.js
+++ js/app.js
@@ -2,6 +2,23 @@
  * Javascript file for 2 cars
  */
 
+// Constants
+var WINDOW_WIDTH = window.innerWidth;
+var WINDOW_HEIGHT = window.innerHeight;
+var BACKGROUND_COLOR = "#25337a";
+var LINE_COLOR = "#6572a7";
+
 window.onload = function () {
-    // Canvas code goes here
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+
+    canvas.width = WINDOW_WIDTH;
+    canvas.height = WINDOW_HEIGHT;
+    canvas.style.backgroundColor = BACKGROUND_COLOR;
+    ctx.fillStyle = LINE_COLOR;
+    ctx.fillRect(Math.floor(WINDOW_WIDTH/2) - 5, 0, 5, WINDOW_HEIGHT);
+    ctx.fillRect(Math.floor(WINDOW_WIDTH/2) - 100, 0, 2, WINDOW_HEIGHT);
+    ctx.fillRect(Math.floor(WINDOW_WIDTH/2) + 100, 0, 2, WINDOW_HEIGHT);
+    ctx.fillRect(Math.floor(WINDOW_WIDTH/2) - 200, 0, 2, WINDOW_HEIGHT);
+    ctx.fillRect(Math.floor(WINDOW_WIDTH/2) + 200, 0, 2, WINDOW_HEIGHT);
 };
