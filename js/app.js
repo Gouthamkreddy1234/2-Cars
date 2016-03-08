7e6bd3552814930c8f78685417307ba1eb83214b
Gouthamkreddy1234 <goutamkreddy@gmail.com>
2016-03-09 05:21:51 +0530

    with obstacles and car movement2

--- js/app.js
+++ js/app.js
@@ -96,10 +96,99 @@ function drawCars() {
     BLUE_CAR_OBJ.draw();
 }
 
+function drawObstacles()
+{
+
+    var radius = 25;
+    ctx.beginPath();
+    ctx.arc(Math.floor(WINDOW_WIDTH / 2) - 150,Math.floor(WINDOW_HEIGHT) - 360,radius,0,2*Math.PI);
+    ctx.fillStyle = '#FE3E67';
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(Math.floor(WINDOW_WIDTH / 2) - 150,Math.floor(WINDOW_HEIGHT) - 360,radius*0.8, 0, 2*Math.PI);
+    ctx.fillStyle = 'white';
+    ctx.fill();//GO
+
+    ctx.beginPath();
+    ctx.arc(Math.floor(WINDOW_WIDTH / 2) - 150,Math.floor(WINDOW_HEIGHT) - 360,radius*0.50, 0, 2*Math.PI);
+    ctx.fillStyle = '#FE3E67';
+    ctx.fill();
+
+
+    ctx.beginPath();
+    ctx.arc(Math.floor(WINDOW_WIDTH / 2) + 150,Math.floor(WINDOW_HEIGHT) - 560,radius,0,2*Math.PI);
+    ctx.fillStyle = '#05A8C4';
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(Math.floor(WINDOW_WIDTH / 2) + 150,Math.floor(WINDOW_HEIGHT) - 560,radius*0.8, 0, 2*Math.PI);
+    ctx.fillStyle = 'white';
+    ctx.fill();//GO
+
+    ctx.beginPath();
+    ctx.arc(Math.floor(WINDOW_WIDTH / 2) + 150,Math.floor(WINDOW_HEIGHT) - 560,radius*0.50, 0, 2*Math.PI);
+    ctx.fillStyle = '#05A8C4';
+    ctx.fill();
+
+
+    //red rect
+    ctx.lineJoin = "round";
+    ctx.lineWidth = 20;
+    ctx.strokeStyle="#FE3E67";
+
+    var xrect=523;
+    var yrect=59;
+    var size=25;
+
+    // Change origin and dimensions to match true size (a stroke makes the shape a bit larger)
+    ctx.strokeRect(xrect, yrect, size,size);
+    ctx.fillStyle="#FE3E67";
+    ctx.fillRect(xrect,yrect, size,size);
+
+
+    ctx.lineWidth = 10;
+    ctx.strokeStyle="white";
+    // Change origin and dimensions to match true size (a stroke makes the shape a bit larger)
+    ctx.strokeRect(xrect+1, yrect+1, size-1.5,size-1.5);
+    ctx.fillStyle="white";
+    ctx.fillRect(xrect+1, yrect+1, size-1.5, size-1.5);
+
+    ctx.fillStyle="#FE3E67";
+    ctx.fillRect(xrect+3, yrect+3, size-6, size-6);
+
+    //blue rect
+
+    ctx.lineJoin = "round";
+    ctx.lineWidth = 20;
+    ctx.strokeStyle="#05A8C4";
+
+    xrect=820;
+    yrect=360;
+    size=25;
+
+    // Change origin and dimensions to match true size (a stroke makes the shape a bit larger)
+    ctx.strokeRect(xrect, yrect, size,size);
+    ctx.fillStyle="#05A8C4";
+    ctx.fillRect(xrect,yrect, size,size);
+
+
+    ctx.lineWidth = 10;
+    ctx.strokeStyle="white";
+    // Change origin and dimensions to match true size (a stroke makes the shape a bit larger)
+    ctx.strokeRect(xrect+1, yrect+1, size-1.5,size-1.5);
+    ctx.fillStyle="white";
+    ctx.fillRect(xrect+1, yrect+1, size-1.5, size-1.5);
+
+    ctx.fillStyle="#05A8C4";
+    ctx.fillRect(xrect+3, yrect+3, size-6, size-6);
+}
+
 function draw() {
     clearCanvas();
     drawLanes();
     drawCars();
+    drawObstacles();
 }
 
 function start() {
